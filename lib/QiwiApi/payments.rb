329726deea3a689b8387348288939e0497645b05
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-06-11 10:48:58 +0600

    Fix parsing of response

--- lib/QiwiApi/payments.rb
+++ lib/QiwiApi/payments.rb
@@ -1,20 +1,24 @@
-
 require "httparty"
 
 module QiwiApi
 	class Client
-			def initialize(access_token = nil)
-					@access_token = access_token
-			end
+		def initialize(access_token)
+			@access_token = access_token
+		end
 
-			def payments(qty, number, operation)
-					url = "https://edge.qiwi.com/payment-history/v2/persons/#{number}/payments?rows=#{qty}&operation=#{operation}"
-					response = HTTParty.get(url, headers: { 'Authorization' => "Bearer #{@access_token}" , 'Accept' => " application/json", 'Content-Type' => 'application/json'})
-					sum = response.to_a
-					amount = sum[0][1][0]["total"]["amount"]
-					date = sum[0][1][0]["date"]
-					comment = sum[0][1][0]["comment"]
-					p amount, date, comment
-				end
+		def payments(qty, number, operation)
+			payments_history = []
+			url = "https://edge.qiwi.com/payment-history/v2/persons/#{number}/payments?rows=#{qty}&operation=#{operation}"
+			response = HTTParty.get(url, headers: { 'Authorization' => "Bearer #{@access_token}" , 'Accept' => " application/json", 'Content-Type' => 'application/json'})
+			response["data"].each do |item|
+				hash = {
+					account: item["account"],
+					amount: item["total"]["amount"],
+					comment: item["comment"]
+				}
+				payments_history << hash
+			end
+			payments_history
+		end
 	end
 end
