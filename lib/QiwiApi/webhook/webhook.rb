665d3893f3656564a86430526f61819c2c5ca33f
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-06-14 00:06:04 +0600

    Change version of gem

---
+++ lib/QiwiApi/webhook/webhook.rb
@@ -0,0 +1,26 @@
+module QiwiApi
+  class Webhook
+    def initialize(key)
+      @key = key
+    end
+
+    def call(params)
+      return WebhookResponse.new(success: true, body: params) if params.dig(:test) == true
+
+      if sign_correct?(params)
+        return WebhookResponse.new(success: true, body: params)
+      else
+        return WebhookResponse.new(success: false, body: params.dig(:payment, :txnId))
+      end
+    end
+
+    private
+
+    def sign_correct?(params)
+      sign_fields = "#{params.dig(:payment, :sum, :currency)}|#{params.dig(:payment, :sum, :amount)}|#{params.dig(:payment, :type)}|#{params.dig(:payment, :account)}|#{params.dig(:payment, :txnId)}"
+      secret_key = Base64.decode64(@key)
+      secure_hash = OpenSSL::HMAC.hexdigest('SHA256', secret_key, sign_fields)
+      params[:hash] == secure_hash
+    end
+  end
+end
---
+++ lib/QiwiApi/webhook/webhook_response.rb
@@ -0,0 +1,32 @@
+module QiwiApi
+  class WebhookResponse < Response
+    attr_reader :error, :value
+    def initialize(success:, body:)
+      @success = success
+      if @success
+        @value = parse_body(body)
+      else
+        @error = error_description(body)
+      end
+    end
+
+    def success?
+      @success
+    end
+
+    private
+
+    def parse_body(body)
+      if body[:test] != true
+        output = body[:payment]
+        return output.select { |key, val| key != :signFields }.to_h
+      else
+        return body
+      end
+    end
+
+    def error_description(body)
+      "This ##{body} transaction is not authenticated"
+    end
+  end
+end
