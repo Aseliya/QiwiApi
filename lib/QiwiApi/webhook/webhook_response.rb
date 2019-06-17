59f9ae77456567a0148d71ab8be6daba8a38850e
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-06-17 12:01:55 +0600

    Fix webhook response

--- lib/QiwiApi/webhook/webhook_response.rb
+++ lib/QiwiApi/webhook/webhook_response.rb
@@ -1,32 +1,15 @@
 module QiwiApi
   class WebhookResponse
-    # attr_reader :error, :value
+
     def initialize(body)
-      # @success = success
-      # if @success
-        @body = parse_body(body)
-      # else
-      #   @error = error_description(body)
-      # end
+      @body = parse_body(body)
     end
 
-    # def success?
-    #   @success
-    # end
-
-    # private
+    private
 
     def parse_body(body)
-      # if body[:test] != true
-        parsed_body = body[:payment]
-        return parsed_body.select { |key, val| key != :signFields }.to_h
-      else
-        return body
-      end
+      parsed_body = body[:payment]
+      return parsed_body.select { |key, val| key != :signFields }.to_h
     end
-
-    # def error_description(body)
-    #   "This ##{body} transaction is not authenticated"
-    # end
   end
 end
